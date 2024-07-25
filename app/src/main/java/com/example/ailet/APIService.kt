12a16d7a84394769b1aaf45d14bd33353b8d17a8
Faraz <Faraz7321@gmail.com>
2024-07-25 13:54:00 +0200

    API service

---
+++ app/src/main/java/com/example/ailet/APIService.kt
@@ -0,0 +1,19 @@
+package com.example.ailet
+
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import retrofit2.Call
+import retrofit2.http.Multipart
+import retrofit2.http.POST
+import retrofit2.http.Part
+
+interface ApiService {
+    @Multipart
+    @POST("api/v2/photos/")
+    fun uploadPhoto(
+        @Part("photo_id") photoId: RequestBody,
+        @Part("visit_id") visitId: RequestBody,
+        @Part("task_id") taskId: RequestBody,
+        @Part photo: MultipartBody.Part
+    ): Call<Void>
+}
